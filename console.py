ae4c14ab8fb5df27f934a0a0b2d850415113b983
ISA SULAIMAN ISA <isasulaimanisa@gmail.com>
2023-12-11 01:10:35 +0100

    console

---
+++ console.py
@@ -0,0 +1,159 @@
+#!/usr/bin/python3
+"""Module defines console CLI for
+HBNB APP
+"""
+import cmd
+from models import storage
+from models.base_model import BaseModel
+
+
+class HBNBCommand(cmd.Cmd):
+    """Creates a CLI for HBNB
+    """
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """Quit command exits the program
+        """
+        return True
+
+    def do_EOF(self, line):
+        """EOF command exits the program
+        """
+        return True
+
+    def emptyline(self):
+        pass
+
+    def do_create(self, line):
+        """Creates an object of specified class
+        Ex: $ create BaseModel
+        """
+        args = line.split()
+        if len(args) < 1:
+            print("** class name missing **")
+        else:
+            try:
+                myobj = eval(args[0])()
+            except Exception:
+                print(f"** class doesn't exist **")
+            else:
+                storage.new(myobj)
+                storage.save()
+
+    def do_show(self, line):
+        """Prints the string representation of an instance
+        Ex: $ show BaseModel 1234-1234-1234
+        """
+        args = line.split()
+        if len(args) < 1:
+            print("** class name missing **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            allobjs = storage.all()
+            classfound = 0
+            for value in allobjs.values():
+                mydict = value.to_dict()
+                if mydict['__class__'] == args[0]:
+                    classfound = 1
+            if classfound:
+                key = f"{args[0]}.{args[1]}"
+                try:
+                    myobj = allobjs[key]
+                except Exception:
+                    print("** no instance found **")
+                else:
+                    print(myobj)
+            else:
+                print("** class doesn't exist **")
+
+    def do_destroy(self, line):
+        """Deletes instance based on class and id
+        Ex: $ destroy BaseModel 1234-1234-1234
+        """
+        args = line.split()
+        if len(args) < 1:
+            print("** class name missing **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            allobjs = storage.all()
+            classfound = 0
+            for value in allobjs.values():
+                mydict = value.to_dict()
+                if mydict['__class__'] == args[0]:
+                    classfound = 1
+            if classfound:
+                key = f"{args[0]}.{args[1]}"
+                try:
+                    del storage.all()[key]
+                except Exception:
+                    print("** no instance found **")
+                else:
+                    storage.save()
+            else:
+                print("** class doesn't exist **")
+
+    def do_all(self, line):
+        """Prints all string representation of all instances
+        Ex: $ all BaseModel or $ all
+        """
+        args = line.split()
+        allobjs = storage.all()
+        mylist = []
+        if len(args) > 0:
+            classfound = 0
+            for value in allobjs.values():
+                myobj = value.to_dict()
+                if myobj['__class__'] == args[0]:
+                    classfound = 1
+                    del myobj['__class__']
+                    mylist.append(str(myobj))
+            if classfound:
+                print(mylist)
+            else:
+                print("** class doesn't exist **")
+        else:
+            for value in allobjs.values():
+                myobj = value.to_dict()
+                del myobj['__class__']
+                mylist.append(str(myobj))
+            print(mylist)
+
+    def do_update(self, line):
+        """Updates instance attriutes
+        Ex: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com"
+        """
+        args = line.split()
+        if len(args) < 1:
+            print("** class name missing **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        elif len(args) < 3:
+            print("** attribute name missing **")
+        elif len(args) < 4:
+            print("** value missing **")
+        else:
+            allobjs = storage.all()
+            classfound = 0
+            for value in allobjs.values():
+                mydict = value.to_dict()
+                if mydict['__class__'] == args[0]:
+                    classfound = 1
+            if classfound:
+                key = f"{args[0]}.{args[1]}"
+                try:
+                    myobj = allobjs[key]
+                except Exception:
+                    print("** no instance found **")
+                else:
+                    setattr(storage.all()[key], args[2], args[3])
+                    storage.save()
+            else:
+                print("** class doesn't exist **")
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,30 @@
+from models.base_model import BaseModel
+import unittest
+from datetime import datetime
+
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        self.myobject = BaseModel()
+        self.myobject2 = BaseModel('name' : "isa", 'age': 26)
+    def tearDown(self):
+        pass
+    def test_id(self):
+        self.assertTrue(len(self.myobject.id) != 0)
+    def test_id_type(self):
+        self.assertTrue(isinstance(self.myobject.id, str))
+    def test_created_at(self):
+        self.assertTrue(isinstance(self.myobject.created_at, datetime))
+    def test_updated_at(self):
+        self.assertTrue(isinstance(self.myobject.updated_at, datetime))
+    def test_return_str(self):
+        self.assertEqual(print(self.myobject),
+                         print(f"[{type(self.myobject).__name__}] ({self.myobject.id}) {self.myobject.__dict__}"))
+    def test_save(self):
+        last_save = self.myobject.updated_at
+        self.myobject.save()
+        self.assertNotEqual(last_save, self.myobject.updated_at)
+    def test_dict_init(self):
+
+
+if __name__ == "__main__":
+    unittest.main()
